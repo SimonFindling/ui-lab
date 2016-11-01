6e77b416b02c8e09ae3eb753d4719dc942f039ec
SimonFindling <simon.findling(at)web.de>
2016-11-02 00:31:46 +0100

    login-microservice

---
+++ login-microservice/src/main/java/de/hska/login/microservice/controller/LoginController.java
@@ -0,0 +1,16 @@
+package de.hska.login.microservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class LoginController {
+
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ResponseEntity<String> login() {
+		return new ResponseEntity<String>("hello world", HttpStatus.OK);
+	}
+}
---
+++ login-microservice/src/test/java/de/hska/login/microservice/LoginMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.hska.login.microservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
