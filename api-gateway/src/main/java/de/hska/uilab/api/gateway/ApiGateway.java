3e5ed1db9432029b25dc40fd6975bf96de9e2c27
SimonFindling <simon.findling(at)web.de>
2016-11-14 19:10:41 +0100

    added autoroutes for in discovery registered services. see ${API-Gateway-URL}/routes.

--- api-gateway/src/main/java/de/hska/uilab/api/gateway/ApiGateway.java
+++ api-gateway/src/main/java/de/hska/uilab/api/gateway/ApiGateway.java
@@ -3,11 +3,11 @@ package de.hska.uilab.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
+@EnableSidecar
 public class ApiGateway {
 
 	public static void main(String[] args) {
