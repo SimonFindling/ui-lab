fae608cc6e6a284e502bfe68cda199b4d808c081
SimonFindling <simon.findling(at)web.de>
2016-11-28 00:16:15 +0100

    added test environment bootstrap.yml and application.yml for spring boot test support.

--- api-gateway/src/test/java/de/hska/uilab/api/gateway/APIGatewayApplicationTests.java
+++ api-gateway/src/test/java/de/hska/uilab/api/gateway/APIGatewayApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LoginMicroserviceApplicationTests {
+public class APIGatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
