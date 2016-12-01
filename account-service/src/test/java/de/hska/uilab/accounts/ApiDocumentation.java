186f161656a50fb74963e0fa148164a2b7185abe
mavogel <mavogel@posteo.de>
2016-12-01 16:57:24 +0100

    Added rest-mvc-doc. Listing all test implemented.

--- account-service/src/test/java/de/hska/uilab/accounts/ApiDocumentation.java
+++ account-service/src/test/java/de/hska/uilab/accounts/ApiDocumentation.java
@@ -1,4 +1,4 @@
-package com.github.mavogel.vislab;
+package de.hska.uilab.accounts;
 /*
  *  The MIT License (MIT)
  *
@@ -26,38 +26,34 @@ package com.github.mavogel.vislab;
  */
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.mavogel.vislab.category.model.Category;
-import com.github.mavogel.vislab.category.repository.CategoryRepository;
+import de.hska.uilab.accounts.model.Account;
+import de.hska.uilab.accounts.model.Service;
+import de.hska.uilab.accounts.repository.AccountRepository;
+import de.hska.uilab.accounts.repository.ServiceRepository;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.crossstore.HashMapChangeSet;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
-import org.springframework.restdocs.constraints.ConstraintDescriptions;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.snippet.Attributes.key;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -65,6 +61,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD) // clear the db before each test
 public class ApiDocumentation {
 
     @Rule
@@ -78,7 +75,10 @@ public class ApiDocumentation {
     private WebApplicationContext context;
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private ServiceRepository serviceRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -94,70 +94,114 @@ public class ApiDocumentation {
     }
 
     @Test
-    public void listCategories() throws Exception {
-        createSampleCategory(1L, "TestCategory");
+    public void listAllAccountsOfTenants() throws Exception {
+        Service product = createService(Service.ServiceName.PRODUCT);
+        Service sales = createService(Service.ServiceName.SALES);
+        Service customer = createService(Service.ServiceName.CUSTOMER);
 
-        this.mockMvc.perform(get("/categories").accept(MediaType.APPLICATION_JSON)
-                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42"))
+        Account sampleTenantAccount1 = createSampleTenantAccount("prospect1@test.org", product, sales, customer);
+        createSampleTenantAccount("prospect2@test.org", product, customer);
+        createSampleUserAccount(sampleTenantAccount1.getId(), "John", "Doe", "user@test.org", product, sales, customer);
+
+        this.mockMvc.perform(get("/accounts").accept(MediaType.APPLICATION_JSON)
+                .header("Authorization: Bearer", "0b79bab50daca910b000d4f1a2b675d604257e42"))
                 .andExpect(status().isOk())
                 .andDo(this.documentationHandler.document(
                         responseFields(
-                                fieldWithPath("[].id").description("The category ID"),
-                                fieldWithPath("[].name").description("The name of the category")
+                                fieldWithPath("[].id").description("The account ID"),
+                                fieldWithPath("[].email").description("the email address of the account"),
+                                fieldWithPath("[].username").description("the username of the account"),
+                                fieldWithPath("[].password").description("the password of the user"),
+                                fieldWithPath("[].tenantStatus").description("the status of the tenant PROSPECT or CUSTOMER, which is empty in ADMIN accounts"),
+                                fieldWithPath("[].tenantId").description("the corresponding tenantId, which is only set in USER accounts"),
+                                fieldWithPath("[].accountType").description("the type of the account: ADMIN, TENANT or USER"),
+                                fieldWithPath("[].firstname").description("the firstname of the account owner"),
+                                fieldWithPath("[].lastname").description("the lastname of the account owner"),
+                                fieldWithPath("[].company").description("the company of the account owner"),
+                                fieldWithPath("[].services.[]").description("the services/modules this account can use")
                         )
                 ));
     }
 
-    @Test
-    public void addCategory() throws Exception {
-        Map<String, String> newCategory = new HashMap<>();
-        newCategory.put("id", "1");
-        newCategory.put("name", "TestCategory");
+    private Service createService(final Service.ServiceName name) {
+        return this.serviceRepository.save(new Service(name));
 
-        ConstrainedFields fields = new ConstrainedFields(Category.class);
-
-        this.mockMvc.perform(post("/categories")
-                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(this.objectMapper.writeValueAsString(newCategory)))
-                .andExpect(status().isCreated())
-                .andDo(this.documentationHandler.document(
-                        requestFields(
-                                fields.withPath("id").description("The category ID"),
-                                fields.withPath("name").description("The name of the category")
-                        )
-                ));
     }
 
-    @Test
-    public void deleteCategory() throws Exception {
-        Category originalCategory = createSampleCategory(1L, "TestCategory");
-
-        this.mockMvc.perform(delete("/categories/" + originalCategory.getId())
-                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
-    }
-
-    /**
-     * HELPERS
-     */
-    private static class ConstrainedFields {
-
-        private final ConstraintDescriptions constraintDescriptions;
-
-        ConstrainedFields(Class<?> input) {
-            this.constraintDescriptions = new ConstraintDescriptions(input);
+    private Account createSampleTenantAccount(final String email, final Service... services) {
+        Account createdAcc = this.accountRepository.save(Account.asProspect(email));
+        if(services != null && services.length > 0) {
+            createdAcc.addServices(services);
+            createdAcc = this.accountRepository.save(createdAcc);
         }
 
-        private FieldDescriptor withPath(String path) {
-            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
-                    .collectionToDelimitedString(this.constraintDescriptions
-                            .descriptionsForProperty(path), ". ")));
-        }
+        return createdAcc;
+
     }
 
-    private Category createSampleCategory(final Long id, final String name) {
-        return categoryRepository.save(new Category(id, name));
+    private Account createSampleUserAccount(final Long tenantId, final String firstName, final String lastName,
+                                            final String email, final Service... services) {
+        Account createdAcc = this.accountRepository.save(Account.asUser(tenantId, firstName, lastName, email));
+
+        if(services != null && services.length > 0) {
+            createdAcc.addServices(services);
+            createdAcc = this.accountRepository.save(createdAcc);
+        }
+
+        return createdAcc;
     }
+
+
+//    @Test
+//    public void addCategory() throws Exception {
+//        Map<String, String> newCategory = new HashMap<>();
+//        newCategory.put("id", "1");
+//        newCategory.put("name", "TestCategory");
+//
+//        ConstrainedFields fields = new ConstrainedFields(Category.class);
+//
+//        this.mockMvc.perform(post("/categories")
+//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(this.objectMapper.writeValueAsString(newCategory)))
+//                .andExpect(status().isCreated())
+//                .andDo(this.documentationHandler.document(
+//                        requestFields(
+//                                fields.withPath("id").description("The category ID"),
+//                                fields.withPath("name").description("The name of the category")
+//                        )
+//                ));
+//    }
+//
+//    @Test
+//    public void deleteCategory() throws Exception {
+//        Category originalCategory = createSampleCategory(1L, "TestCategory");
+//
+//        this.mockMvc.perform(delete("/categories/" + originalCategory.getId())
+//                .header("Authorization: Basic", "0b79bab50daca910b000d4f1a2b675d604257e42")
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    /**
+//     * HELPERS
+//     */
+//    private static class ConstrainedFields {
+//
+//        private final ConstraintDescriptions constraintDescriptions;
+//
+//        ConstrainedFields(Class<?> input) {
+//            this.constraintDescriptions = new ConstraintDescriptions(input);
+//        }
+//
+//        private FieldDescriptor withPath(String path) {
+//            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+//                    .collectionToDelimitedString(this.constraintDescriptions
+//                            .descriptionsForProperty(path), ". ")));
+//        }
+//    }
+//
+//    private Category createSampleCategory(final Long id, final String name) {
+//        return categoryRepository.save(new Category(id, name));
+//    }
 }
