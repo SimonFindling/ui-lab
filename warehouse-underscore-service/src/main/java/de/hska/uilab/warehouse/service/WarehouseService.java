7021affec853cd863802ba3a4d89d62efe6628f1
Duniel1 <danielbenz1@gmx.net>
2017-01-04 22:24:25 +0100

    added call to product service for testing.

--- warehouse-underscore-service/src/main/java/de/hska/uilab/warehouse/service/WarehouseService.java
+++ warehouse-underscore-service/src/main/java/de/hska/uilab/warehouse/service/WarehouseService.java
@@ -5,17 +5,27 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import de.hska.uilab.warehouse.data.Product;
 import de.hska.uilab.warehouse.data.Warehouse;
 
 @RestController
 public class WarehouseService {
 	
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Autowired
+	RestTemplate restTemplate;
 
 	private final static Logger LOGGER = Logger.getLogger(WarehouseService.class.getName());
 
@@ -31,6 +41,10 @@ public class WarehouseService {
 	@RequestMapping(value = "warehouse", method = RequestMethod.GET)
 	public ResponseEntity<List<Warehouse>> getWarehouses(){
 		LOGGER.log(Level.INFO, "get all warehouses");
+		
+		ResponseEntity<Product[]> responseEntity = this.restTemplate.getForEntity("http://172.19.0.9:7654/products", Product[].class);
+		Product[] objects = responseEntity.getBody();
+		LOGGER.log(Level.INFO, objects+"");
 		return new ResponseEntity<List<Warehouse>>(dbh.getAllWarehouses(), HttpStatus.OK);
 	}
 }
